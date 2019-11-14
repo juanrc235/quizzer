bdbb66cd1df8d59c259920668c04a97333beb59d
juanrc235 <juan.romero2@alu.uclm.es>
2019-11-14 10:09:07 +0100

    Add load a question file

--- main.py
+++ main.py
@@ -9,6 +9,21 @@ paths = ['questions/1.1.txt',
          'questions/2.2.txt',
          'questions/3.1.txt']
 
+def usage():
+  print('Usage: main.py: Use preload questions\n       main.py <-f> <file>: Use the questions of the file')
+
+def parse_argv(argv):
+
+    if len(argv) == 1:
+        selection = 1
+    elif len(argv) == 3 and argv[1] == '-f':
+        selection = 2
+    else:
+        selection = 3
+
+    return selection
+
+
 def create_unit(path):
     with open(path, 'r', encoding='utf-8') as fd:
         content = fd.read()
@@ -47,20 +62,39 @@ def ask(unit, question_list):
         if question.your_answer is not None:
             print('{} / {}'.format(question.your_answer, question.question))
 
-units = []
-for path in paths:
-    units.append(create_unit(path))
+def preload_questions():
+    units = []
+    for path in paths:
+        units.append(create_unit(path))
 
-print('\nWELCOME TO QUIZZER. YOUR PLACE IF YOU LOVE ISO\nPress [Ctrl + c] to exit\n')
+    print('\nWELCOME TO QUIZZER. YOUR PLACE IF YOU LOVE ISO\nPress [Ctrl + c] to exit\n')
 
-str_units = [unit[0].split(' ')[1] for unit in units]
-str = ', '.join(str_units)
-selection = ''
-while selection not in str_units:
-    try:
-        selection = input('Select the Unit [' + str  + ']: ')
-    except KeyboardInterrupt:
-        print('\nBye, Bye')
-        sys.exit(0)
+    str_units = [unit[0].split(' ')[1] for unit in units]
+    str = ', '.join(str_units)
+    selection = ''
+    while selection not in str_units:
+        try:
+            selection = input('Select the Unit [' + str  + ']: ')
+        except KeyboardInterrupt:
+            print('\nBye, Bye')
+            sys.exit(0)
 
-ask(units[u[selection]][0], units[u[selection]][1])
+    ask(units[u[selection]][0], units[u[selection]][1])
+
+def load_file(path):
+    unit = create_unit(path)
+    ask(unit[0], unit[1])
+
+def main(argv):
+
+    selection = parse_argv(argv)
+
+    if selection == 1:
+        preload_questions()
+    elif selection == 2:
+        load_file(argv[2])
+    else:
+        usage()
+
+if __name__ =='__main__':
+    main(sys.argv)
