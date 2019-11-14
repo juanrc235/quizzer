fcf13516a34994992c06eabf36ec57e46be00d5b
juanrc235 <juan.romero2@alu.uclm.es>
2019-11-14 09:36:03 +0100

    Add 3.1 questions. Improve main script

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ from question import Question
 import random
 import sys
 
-u = {'1.1' : 0, '1.2' : 1, '2.1' : 2, '2.2' : 3}
+u = {'1.1' : 0, '1.2' : 1, '2.1' : 2, '2.2' : 3, '3.1' : 4}
 paths = ['questions/1.1.txt',
          'questions/1.2.txt',
          'questions/2.1.txt',
-         'questions/2.2.txt']
+         'questions/2.2.txt',
+         'questions/3.1.txt']
 
 def create_unit(path):
     with open(path, 'r', encoding='utf-8') as fd:
@@ -23,7 +24,11 @@ def create_unit(path):
     question_list = []
     i = 1
     for item in final:
-        question_list.append( Question( i, item[0], item[1], (item[2], item[3], item[4])) )
+        try:
+            question_list.append( Question( i, item[0], item[1], (item[2], item[3], item[4])) )
+        except Exception:
+            print('[ERROR] File: {} Question: {}'.format(path, i, item[0]))
+            sys,exit()
         i+=1
 
     random.shuffle(question_list)
@@ -37,7 +42,7 @@ def ask(unit, question_list):
             question.ask()
         except KeyboardInterrupt:
             break
-    print('\n| ================= RESULTs =================| ')
+    print('\n| ================= RESULTS =================| ')
     for question in question_list:
         if question.your_answer is not None:
             print('{} / {}'.format(question.your_answer, question.question))
@@ -48,10 +53,12 @@ for path in paths:
 
 print('\nWELCOME TO QUIZZER. YOUR PLACE IF YOU LOVE ISO\nPress [Ctrl + c] to exit\n')
 
+str_units = [unit[0].split(' ')[1] for unit in units]
+str = ', '.join(str_units)
 selection = ''
-while selection not in ('1.1', '1.2', '2.1', '2.2'):
+while selection not in str_units:
     try:
-        selection = input('Select the Unit [1.1, 1.2, 2.1, 2.2]: ')
+        selection = input('Select the Unit [' + str  + ']: ')
     except KeyboardInterrupt:
         print('\nBye, Bye')
         sys.exit(0)
